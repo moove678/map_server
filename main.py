487ba4da71210f718cb8128fb692c8023501c59c
user <shcal20@gmail.com>
2025-04-08 06:45:22 +0300

    group

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_migrate import Migrate migrate = Migrate(app, db)
 
 # --- CONFIG ---
 app = Flask(__name__)
