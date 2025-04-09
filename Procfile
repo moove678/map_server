1da96825efd9b89121dab618718aef4f418f6040
user <shcal20@gmail.com>
2025-04-09 12:03:57 +0300

    added start script and connected DB via env

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bash start.sh
+web: python manage.py db upgrade && gunicorn main:app
--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo "Running migrations..."
-python manage.py db upgrade
-echo "Starting server..."
 gunicorn main:app
