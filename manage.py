2bcda762d0c2362dff715fad0aa834172ecada34
user <shcal20@gmail.com>
2025-04-09 11:21:28 +0300

    init

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 from flask.cli import FlaskGroup
-from main import app, db
 
-cli = FlaskGroup(app)
+from main import app, db  # Убедись, что у тебя файл называется main.py
+
+cli = FlaskGroup(create_app=lambda: app)
 
 if __name__ == '__main__':
     cli()
